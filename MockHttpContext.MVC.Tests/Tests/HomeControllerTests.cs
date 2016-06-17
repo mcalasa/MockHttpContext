fc6b3db7311aedc842d342a4ce36b9fc7a1ae7cc
unknown <mcalasa@WLA-CALAS7010.hlhz.net>
2016-06-17 13:37:58 -0700

    Created real simple controller test for MVC

---
+++ MockHttpContext.MVC.Tests/Tests/HomeControllerTests.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+using MockHttpContext;
+using Xunit;
+
+namespace MockHttpContext.MVC.Tests.Tests
+{
+    public class HomeControllerTests
+    {
+        [Fact]
+        public void IndexControllerReturnsQueryString()
+        {
+            //Setup the controller to be tested
+            var controller = new MockHttpContext.MVC.Controllers.HomeController();
+            
+            //Setup the context
+            var mockedHttpContext = new HttpContext("SomeQueryStringValue", @"contoso\bob");
+
+            //Pass the mocked context to the controller
+            controller.ControllerContext = new ControllerContext(mockedHttpContext.CurrentHttpContext(), new System.Web.Routing.RouteData(), controller);
+            
+            Assert.Equal("SomeQueryStringValue", controller.Request.QueryString["qsValue"]);            
+        }
+    }
+}
